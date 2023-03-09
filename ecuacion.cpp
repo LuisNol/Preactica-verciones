712c1447b6d029d80cfa5329b8a5d394144af71d
Nolberto Luis Sumaran Pimentel <101943527+LuisNol@users.noreply.github.com>
2023-03-09 16:36:31 -0500

    Version v.03

--- ecuacion.cpp
+++ ecuacion.cpp
@@ -5,7 +5,12 @@ using namespace std;
 
 
 int main(){
-
+     int count = 0;
     cout<<"hola como estas  captura  ";
+    for (size_t i = 0; i < count; i++)
+    {
+        cout<<" "<<i<<;
+    }
+    
     return 0;
 }
